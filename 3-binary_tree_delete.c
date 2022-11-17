2b89ca534c2f236b917d2d627dd96df70cb78d0a
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-11-17 02:59:31 +0000

    Update solution to task 3 - betty styles

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,13 +2,12 @@
 
 /**
  * binary_tree_delete - deletes an entire binary tree
- * 
  * @tree: a pointer to the root node of the tree to delete
- * 
  * Description: If tree is NULL, do nothing
  */
 
-void binary_tree_delete(binary_tree_t *tree){
+void binary_tree_delete(binary_tree_t *tree)
+{
 	if (!tree)
 		return;
 	binary_tree_delete(tree->right);
